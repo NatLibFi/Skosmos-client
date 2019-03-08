523e1ad3d8218603957ffe81c054dceaaa759300
Osma Suominen <osma.suominen@helsinki.fi>
2019-03-08 12:11:50 +0200

    Bump version: 0.2.2 → 0.2.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='skosmos-client',
-    version='0.2.2',
+    version='0.2.3',
     url='https://github.com/NatLibFi/Skosmos-client',
     author='Osma Suominen',
     author_email='osma.suominen@helsinki.fi',
